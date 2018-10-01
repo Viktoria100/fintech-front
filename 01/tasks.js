910519f2c99d9a2a5633cc684ab4f54d2c68c3c3
Viktoria100 <43489529+Viktoria100@users.noreply.github.com>
2018-10-01 14:56:09 +0700

    1 домашнее задание+1 задача из того, что не вошло в релиз.

--- 01/tasks.js
+++ 01/tasks.js
@@ -8,7 +8,20 @@
  * @return {{min: number, max: number}} объект с минимумом и максимумом
  * '1 и 6.45, -2, но 8, а затем 15, то есть 2.7 и -1028' => { min: -1028, max: 15 }
  */
-function getMinMax(string) {}
+function getMinMax(string)
+{
+    {function getMaxOfArray(numArray)
+    {
+        return Math.max.apply(null, numArray);
+    }
+        function getMinOfArray(numArray)
+        {
+            return Math.min.apply(null, numArray);
+        }
+        const Arr = (string.split(/[А-я, !-, ,:-~ , / ]/));
+        console.log('min:'+' '+ getMinOfArray(Arr));
+        console.log('max:' +' '+ getMaxOfArray(Arr));
+}
 
 /* ============================================= */
 
@@ -17,8 +30,12 @@ function getMinMax(string) {}
  * @param {number} x номер числа
  * @return {number} число под номером х
  */
-function fibonacciSimple(x) {
-  return x;
+function fibonacciSimple(x)
+{
+    if (x==0||x==1)
+        return x;
+    else
+        return fibonacciSimple(x-2)+fibonacciSimple(x-1);
 }
 
 /* ============================================= */
@@ -29,8 +46,25 @@ function fibonacciSimple(x) {
  * @param {number} x номер числа
  * @return {number} число под номером х
  */
-function fibonacciWithCache(x) {
-  return x;
+let cache= new Map();
+function fibonacciWithCache(x)
+{
+    if(cache.has(x)==true)
+        return cache.get(x);
+    else
+      {
+        if (x == 0 || x == 1)
+        {
+            cache.set(x, x);
+            return x;
+        }
+        else
+          {
+            let f = fibonacciWithCache(x - 2) + fibonacciWithCache(x - 1);
+            cache.set(x, f);
+            return f;
+          }
+      }
 }
 
 /* ============================================= */
@@ -50,7 +84,32 @@ function fibonacciWithCache(x) {
  * @param  {number} cols количество столбцов
  * @return {string}
  */
-function printNumbers(max, cols) {}
+function printNumbers(max, cols)
+{
+    let lines;
+    let k=0;
+    if ((max+1)% cols!=0)
+        lines=Math.trunc((max+1)/cols)+1;
+    else
+        lines=(max+1)/cols;
+    let x=new Array(lines);
+    for (let i=0; i<lines;i++)
+        x[i]=new Array(cols);
+
+    for (let i=0; i<cols;i++)
+    {
+        for (let j = 0; j < lines; j++)
+        {
+            if(k<=max)
+            {x[j][i] = k;
+                k++;}
+        }
+    }
+
+    for (let i=0; i<lines;i++)
+        console.log(x[i].join(' '));
+}
+
 
 /* ============================================= */
 
@@ -59,7 +118,40 @@ function printNumbers(max, cols) {}
  * @param  {string} value
  * @return {string}
  */
-function rle(input) {}
+function rle(input)
+{
+    let output=[];
+    let k=1;
+    for(let i=1; i<=input.length;i++)
+    {
+        if(input[i-1]==input[i])
+        {
+            k++;
+        }
+        else
+        {
+            output.push(input[i-1]);
+            if(k!=1)
+            {
+                output.push(k);
+            }
+            k=1;
+        }
+    }
+    console.log(output.join(''));
+
+    //не работает, если элементы повторяются
+    /* for (let i=0;i<input.length; i++)
+    {
+       let k = input.lastIndexOf(input[i]);
+        output.push(input[i]);
+        if(k!=i)
+        {
+            output.push(k + 1-i);
+            i = i + (k-i);
+        }
+    }*/
+}
 
 module.exports = {
   getMinMax,
@@ -85,7 +177,35 @@ module.exports = {
 /**
  * Игра продолжается, пока пользователь не угадает. После этого выводит в консоль результат.
  */
-function guessNumberA() {}
+function guessNumberA()
+{
+    function getRandomInt(min, max)
+    {
+        return Math.floor(Math.random() * (max - min)) + min;
+    }
+    let random=getRandomInt(1,101);
+    let number = Number(window.prompt("Введи целое число от 1 до 100 :"));
+
+    while (number!=random)
+    {
+        number=Number(prompt("Введи целое число от 1 до 100 :"));
+        if (number.toLowerCase() > random)
+        {
+            alert("Слишком много");
+            number=prompt("Попоробуй еще");
+        }
+        if (number.toLowerCase() < random)
+        {
+            alert("Слишком мало");
+            number=prompt("Попоробуй еще");
+        }
+    }
+    if(number==random)
+    {
+        alert("В точку!");
+        console.log(random);
+    }
+}
 
 /**
  * По завершению игры пользователю предлагается сыграть еще раз. После каждого тура выводится последний и лучший результаты.
