a75ef35cc698fc40974cfdf794c84f3ad994104b
Viktoria100 <43489529+Viktoria100@users.noreply.github.com>
2018-10-01 22:22:56 +0700

    хз

--- decision.js
+++ decision.js
@@ -1,14 +1,15 @@
 //1 задание
-{function getMaxOfArray(numArray)
-{
-    return Math.max.apply(null, numArray);
-}
-    function getMinOfArray(numArray)
-    {
-        return Math.min.apply(null, numArray);
-    }
+
     function getMinMax(string)
     {
+        {function getMaxOfArray(numArray)
+        {
+            return Math.max.apply(null, numArray);
+        }
+        function getMinOfArray(numArray)
+        {
+            return Math.min.apply(null, numArray);
+        }
         const Arr = (string.split(/[А-я, !-, ,:-~ , / ]/));
 //console.log(Arr)
         console.log('min:'+' '+ getMinOfArray(Arr));
